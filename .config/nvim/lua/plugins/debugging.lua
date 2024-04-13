5085d5ce185e363ee21baa2f6ecbe62c2eeb270f
Matthew Bosomworth <m.b.bosomworth@gmail.com>
2024-04-13 13:33:43 -0600

    added nvim config

---
+++ .config/nvim/lua/plugins/debugging.lua
@@ -0,0 +1,46 @@
+return {
+	{
+		"mfussenegger/nvim-dap",
+		dependencies = {
+			"nvim-neotest/nvim-nio",
+			"rcarriga/nvim-dap-ui",
+		},
+    event = "VeryLazy",
+		config = function()
+			local dap = require("dap")
+			local dapui = require("dapui")
+
+			require("dapui").setup()
+
+			dap.listeners.before.attach.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				dapui.close()
+			end
+
+			vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, {})
+			vim.keymap.set("n", "<leader>dc", dap.continue, {})
+		end,
+	},
+	{
+		"jay-babu/mason-nvim-dap.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"mfussenegger/nvim-dap",
+		},
+		config = function()
+			require("mason-nvim-dap").setup({
+				handlers = {},
+				ensure_installed = { "codelldb" },
+			})
+		end,
+	},
+}
