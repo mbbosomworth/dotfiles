5c427ec880ce95d1dc1bcf787454cb4b868b6929
Matthew Bosomworth <m.b.bosomworth@gmail.com>
2024-09-03 12:29:07 -0600

    dependency added for eslint_d

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -7,10 +7,10 @@ return {
 			sources = {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.diagnostics.eslint_d,
 				null_ls.builtins.formatting.clang_format,
 				null_ls.builtins.formatting.black,
 				null_ls.builtins.formatting.isort,
+        require("none-ls.diagnostics.eslint_d")
 				-- null_ls.builtins.formatting.prettierd,
 				-- null_ls.builtins.formatting.markdownlint,
 			},
