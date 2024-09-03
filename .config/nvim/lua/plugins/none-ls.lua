c67eb65425bda6f25d42f260cfd3fecc23abd971
Matthew Bosomworth <m.b.bosomworth@gmail.com>
2024-09-03 12:19:28 -0600

    auto install for tree-sitter and updated none-ls formatting and diagnostics

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -6,13 +6,13 @@ return {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.clang_format,
 				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.diagnostics.eslint_d,
+				null_ls.builtins.formatting.clang_format,
 				null_ls.builtins.formatting.black,
 				null_ls.builtins.formatting.isort,
 				-- null_ls.builtins.formatting.prettierd,
-				null_ls.builtins.formatting.markdownlint,
-				--null_ls.builtins.diagnostics.eslint_d,
+				-- null_ls.builtins.formatting.markdownlint,
 			},
 		})
 
